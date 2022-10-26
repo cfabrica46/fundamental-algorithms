314828305b946b0abea993114f034c82f4556ba3
César Caycho <cfabrica46@gmail.com>
2022-10-25 22:10:09 -0500

    doc: best comment reverse array

--- reverse-array/main.go
+++ reverse-array/main.go
@@ -7,24 +7,15 @@ func main() {
 
 	fmt.Println(arr)
 	fmt.Println(reverseArray(arr))
-	fmt.Println(otherReverse(arr))
 }
 
-// reverseArray repite el ciclo n veces en donde n es la longitud
+// Se requiere de un arreglo ordenado
+
+// reverseArray repite el ciclo n/2 veces en donde n es la longitud
 // del arreglo original
+
+// [1,2,3,4,5,6,7] => [7,2,3,4,5,6,1] => [7,6,3,4,5,2,1] => [7,6,5,4,3,2,1]
 func reverseArray(a []int32) []int32 {
-	var newArr []int32
-
-	for i := len(a) - 1; i >= 0; i-- {
-		newArr = append(newArr, a[i])
-	}
-
-	return newArr
-}
-
-// otherReverse repite el ciclo n/2 veces en donde n es la longitud
-// del arreglo original
-func otherReverse(a []int32) []int32 {
 	// i toma el valor del valor en la mitad menor del arreglo original
 	for i := len(a)/2 - 1; i >= 0; i-- {
 		// opp toma el valor de el opuesto del i esto debido a la siguinte propiedad
@@ -38,4 +29,3 @@ func otherReverse(a []int32) []int32 {
 
 	return a
 }
-
